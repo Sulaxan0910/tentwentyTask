7a612e356a7099e4008f3f2995639bff70e47012
Sulaxan_Gawas <sulaxan.gawas@infi-pre.com>
2024-10-31 22:50:48 +0530

    cleaned and optimized code with linting

--- src/app/components/image-carousel/image-carousel.component.ts
+++ src/app/components/image-carousel/image-carousel.component.ts
@@ -1,6 +1,5 @@
-import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-image-carousel',
@@ -10,27 +9,102 @@ import { isPlatformBrowser } from '@angular/common';
   styleUrl: './image-carousel.component.scss',
 })
 export class ImageCarouselComponent {
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
-  ngOnInit(): void {}
+  images = [
+    {
+      url: '/assets/images/hero-image.webp',
+      alt: '',
+      title: 'Client 1',
+      description: 'Abu Dhabi, United Arab Emirates',
+    },
+    {
+      url: '/assets/images/hero-image-1.webp',
+      alt: '',
+      title: 'Client 2',
+      description: 'Abu Dhabi, United Arab Emirates',
+    },
+    {
+      url: '/assets/images/hero-image.webp',
+      alt: '',
+      title: 'Client 3',
+      description: 'Abu Dhabi, United Arab Emirates',
+    },
+    {
+      url: '/assets/images/hero-image-1.webp',
+      alt: '',
+      title: 'Client 4',
+      description: 'Abu Dhabi, United Arab Emirates',
+    },
+    {
+      url: '/assets/images/hero-image.webp',
+      alt: '',
+      title: 'Client 5',
+      description: 'Abu Dhabi, United Arab Emirates',
+    },
+    {
+      url: '/assets/images/hero-image-1.webp',
+      alt: '',
+      title: 'Client 6',
+      description: 'Abu Dhabi, United Arab Emirates',
+    },
+    {
+      url: '/assets/images/hero-image.webp',
+      alt: '',
+      title: 'Client 7',
+      description: 'Abu Dhabi, United Arab Emirates',
+    },
+    {
+      url: '/assets/images/hero-image-1.webp',
+      alt: '',
+      title: 'Client 8',
+      description: 'Abu Dhabi, United Arab Emirates',
+    },
+  ];
   rotateVal = 0;
   intervalId: any;
   dragStartXAxis = 0;
-  dragstart(event: any) {
-    this.dragStartXAxis = event.clientX;
+  circleX: number = 125;
+  circleY: number = 150;
+  currentImage: any;
+  currentImageIndex = 0;
+
+  constructor() {
+    this.currentImage = this.images[this.currentImageIndex];
   }
-  dragend(event: any) {
-    if (event.clientX - this.dragStartXAxis < 0) this.rotateimageBasedonDrag(0);
-    else this.rotateimageBasedonDrag(1);
+
+  dragstart(event: any, image: any, deviceType = 'desktop') {
+    if (deviceType != 'desktop') {
+      this.dragStartXAxis = event.changedTouches[0].clientX;
+    } else {
+      this.dragStartXAxis = event.clientX;
+    }
   }
-  rotateimageBasedonDrag(direction: number) {
+  dragend(event: any, image: any, deviceType = 'desktop') {
+    let startPointEndPontDiff;
+    if (deviceType != 'desktop') {
+      startPointEndPontDiff =
+        event.changedTouches[0].clientX - this.dragStartXAxis;
+    } else {
+      startPointEndPontDiff = event.clientX - this.dragStartXAxis;
+    }
+    if (startPointEndPontDiff < 0) {
+      this.currentImage = this.images[++this.currentImageIndex % 8];
+      this.rotateimageBasedonDrag(0, startPointEndPontDiff);
+    } else {
+      console.log(this.currentImageIndex);
+      if (this.currentImageIndex == 0) {
+        this.currentImageIndex = 7;
+        this.currentImage = this.images[this.currentImageIndex];
+      } else this.currentImage = this.images[this.currentImageIndex];
+      this.rotateimageBasedonDrag(1, startPointEndPontDiff);
+    }
+  }
+  rotateimageBasedonDrag(direction: number, difference: number) {
     if (direction) {
       this.rotateVal += 45;
     } else {
       this.rotateVal -= 45;
     }
   }
-  circleX: number = 125;
-  circleY: number = 150;
 
   onMouseMove(event: MouseEvent) {
     const target = event.currentTarget as HTMLElement;
